1d74501df598f962b44e0029365326f37d358805
LEON AUGUSTO OKIDA GONCALVES <laog19@inf.ufpr.br>
2022-05-12 10:59:47 -0300

    [fix] - reformulação da função de merge

--- Makefile
+++ Makefile
@@ -1,4 +1,7 @@
-all: meuAlocador.o
+all: avalia meuAlocador.o
+
+avalia: meuAlocador.o
+	gcc -g avalia.c meuAlocador.o -o avalia -no-pie
 
 meuAlocador.o: meuAlocador.s
 	gcc -c -g meuAlocador.s -o meuAlocador.o
--- meuAlocador.s
+++ meuAlocador.s
@@ -7,7 +7,6 @@
     topoInicialHeap: .quad 0
     topoAtualHeap: .quad 0
     ultimoAlocado: .quad 0
-    ultimoLiberado: .quad 0
     .equ OCUPADO, 0
     .equ LIVRE, 1
 
@@ -29,7 +28,6 @@ iniciaAlocador:
     movq %rax, topoInicialHeap
     movq %rax, topoAtualHeap
     movq $0, ultimoAlocado
-    movq $0, ultimoLiberado
     # movq $strInicia, %rdi # primeiro argumento printf (string)
     # movq %rax, %rsi # segundo argumento printf (endereço a ser impresso)
     # call printf
@@ -56,77 +54,103 @@ alocaMem:
     je aumentaHeap
 
     cmpq $LIVRE, (%r10)
-    je armazena
+    jne inicioIteracao
 
-    movq topoInicialHeap, %r10 # itera começando pelo início
-    addq $8, %r10
-    movq ultimoAlocado, %r11 # r11 guarda último alocado
+    cmpq 8(%r10), %rdi
+    jle armazena
 
-    iteracao:
-        cmpq %r10, %r11 # compara iterador com último alocado (já verificado)
-        je aumentaHeap # se igual, aumenta heap
-        
-        movq 8(%r10), %r12
-        cmpq $OCUPADO, (%r10) # verifica se bloco está ocupado
-        je incrementaIterador
+    inicioIteracao:
+        movq topoInicialHeap, %r10 # itera começando pelo início
+        addq $8, %r10
+        movq ultimoAlocado, %r11 # r11 guarda último alocado
 
-        cmpq %r12, %rdi # compara tamanhos de memória
-        jle armazena # armazena no bloco se o tamanho é menor ou igual
+        iteracao:
+            cmpq %r10, %r11 # compara iterador com último alocado (já verificado)
+            je aumentaHeap # se igual, aumenta heap
+            
+            movq 8(%r10), %r12
+            cmpq $OCUPADO, (%r10) # verifica se bloco está ocupado
+            je incrementaIterador
 
-    incrementaIterador:
-        addq $16, %r10
-        addq %r12, %r10
-        jmp iteracao
+            cmpq %r12, %rdi # compara tamanhos de memória
+            jle armazena # armazena no bloco se o tamanho é menor ou igual
 
-    armazena:
-        movq $OCUPADO, (%r10) # indica que bloco agora está ocupado
-        addq $16, %r10 # r10 <- vetor[0]
-        movq %r10, %rax
+        incrementaIterador:
+            addq $16, %r10
+            addq %r12, %r10
+            jmp iteracao
+
+        armazena:
+            movq $OCUPADO, (%r10) # indica que bloco agora está ocupado
+            addq $16, %r10 # r10 <- vetor[0]
+            movq %r10, %rax
+            popq %r12
+            popq %rbp
+            ret
+
+        aumentaHeap:
+            movq topoAtualHeap, %r11
+            movq topoAtualHeap, %r10
+            addq $8, %r10
+            addq $16, %r11
+            addq %rdi, %r11
+            movq %r11, topoAtualHeap
+            pushq %r13
+            movq %rdi, %r13
+
+            movq %r11, %rdi
+            movq $12, %rax
+            syscall
+
+            movq %r10, ultimoAlocado
+            movq $OCUPADO, (%r10) # indica que bloco está ocupado
+            movq %r13, 8(%r10) # armazena tamanho do bloco
+            movq %r10, %rax
+            addq $16, %rax # %rax <- vetor[0]
+            movq %r13, %rdi
+            popq %r13
+
         popq %r12
         popq %rbp
         ret
-
-    aumentaHeap:
-        movq topoAtualHeap, %r11
-        movq topoAtualHeap, %r10
-        addq $8, %r10
-        addq $16, %r11
-        addq %rdi, %r11
-        movq %r11, topoAtualHeap
-        pushq %r13
-        movq %rdi, %r13
-
-        movq %r11, %rdi
-        movq $12, %rax
-        syscall
-
-        movq %r10, ultimoAlocado
-        movq $OCUPADO, (%r10) # indica que bloco está ocupado
-        movq %r13, 8(%r10) # armazena tamanho do bloco
-        movq %r10, %rax
-        addq $16, %rax # %rax <- vetor[0]
-        movq %r13, %rdi
-        popq %r13
-
-    popq %r12
-    popq %rbp
-    ret
 
 merge: # começa pelo espaço de controle de disponibilidade dos blocos
     pushq %rbp
     movq %rsp, %rbp
 
-    movq 8(%rsi), %r10 # segundo bloco
-    movq 8(%rdi), %r11 # primeiro bloco
-    addq %r10, %r11 # soma tamanhos
-    addq $16, %r11 # soma 16
-    movq %r11, 8(%rdi) # guarda no controle de tamanho do primeiro bloco
+    movq topoInicialHeap, %r10
+    addq $8, %r10
 
-    cmpq ultimoAlocado, %rsi # se o segundo é o ultimoAlocado
-    jne fimMerge
-    movq %rdi, ultimoAlocado # coloca o primeiro em ultimoAlocado
+    movq %r10, %r11
+    addq 8(%r11), %r11
+    addq $16, %r11
 
-    fimMerge:
+    lacoMerge:
+        cmpq topoAtualHeap, %r11
+        jg fimLacoMerge
+        cmpq $LIVRE, (%r10)
+        jne elseMerge
+        cmpq $LIVRE, (%r11)
+        jne elseMerge
+        fazOMerge:
+            pushq %r12
+            movq 8(%r11), %r12
+            addq $16, %r12
+            addq 8(%r10), %r12
+            movq %r12, 8(%r10)
+            popq %r12
+            jmp proxMerge
+        elseMerge:
+            movq %r11, %r10
+            addq 8(%r11), %r11
+            addq $16, %r11
+            jmp lacoMerge
+        proxMerge:
+            addq 8(%r11), %r11
+            addq $16, %r11
+            jmp lacoMerge
+
+    fimLacoMerge:
         popq %rbp
         ret
 
@@ -139,39 +163,7 @@ liberaMem:
 
     movq $LIVRE, -16(%rdi) # sinaliza como livre
 
-    movq %rdi, %r10
-    addq -8(%r10), %r10 # r10 <- próximo bloco
-    cmpq %r10, topoAtualHeap # testa se o próximo bloco existe (não passa do topo)
-    jge pula
-    cmpq $LIVRE, (%r10) # se o próximo estiver ocupado, não faz nada
-    jne pula
-    pushq %rdi
-    subq $16, %rdi
-    movq %r10, %rsi
-    call merge # chama merge com o primeiro bloco sendo o atual e o segundo sendo o próximo
-    popq %rdi
-
-    pula:
-        movq ultimoLiberado, %r10
-        cmpq $0, %r10 # testa se existe ultimoLiberado
-        je fim
-        cmpq $OCUPADO, (%r10) # testa se ultimoLiberado já está ocupado
-        je fim
-        addq 8(%r10), %r10
-        addq $16, %r10 # r10 <- bloco seguinte ao ultimoLiberado
-        movq %rdi, %r11
-        subq $16, %r11
-        cmpq %r11, %r10 # testa se o bloco atual é o próximo ao ultimoLiberado
-        jne fim
-        pushq %rdi
-        movq %r11, %rsi
-        movq ultimoLiberado, %rdi
-        call merge # chama merge com o primeiro bloco sendo o ultimoLiberado e o segundo o atual
-        popq %rdi
-        jmp retornoLibera
-    fim:
-        subq $16, %rdi
-        movq %rdi, ultimoLiberado # atualiza ultimoLiberado com o bloco atual
+    call merge
     retornoLibera:
         popq %rbp
         ret
